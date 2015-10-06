060b4fd8b1efdc6b5b0ac3600f83fd40e5a93b05
alexland <doug.ybarbo@gmail.com>
2015-10-06 00:15:32 -0700

    converted linear flow to graph

--- src/main/scala/org/dougybarbo/EntityResolver.scala
+++ src/main/scala/org/dougybarbo/EntityResolver.scala
@@ -41,11 +41,10 @@ object dataMock {
 
 	def genRawEntity():RawEntity = {
 		val genTickSym = (c:Int) => Seq.fill(c)(RND.alphanumeric(10))
-																	.mkString
-																	.toLowerCase
-
+			.mkString
+			.toLowerCase
 		val d = "a b c d e f g h i j k l m n o p q r s t u v w x y z"
-							.split(" ")
+			.split(" ")
 		val genDba = (c:Int) => Seq.fill(c)(d(RND.nextInt(25))).mkString
 
 		RawEntity(
@@ -71,37 +70,76 @@ object EntityResolver extends App {
 	import actorSystem.dispatcher
 	implicit val flowMaterializer = ActorMaterializer()
 
-	val fnx = (x:List[String]) => List(x.head, x.reverse.head.toLowerCase)
+	val fnx = (x:List[String]) => List(x.head, x.reverse.head.toUpperCase)
 
-	val rawEntities = Source(
+	val rawEntities:Source[RawEntity, Unit] = Source(
 											(1 to 100).map( _ => dataMock.genRawEntity())
-										)
+	)
 
-	val transform1 = Flow[RawEntity]
-										.map(c => c.record.split(" ").toList)
-										.map(fnx(_))
-										.collect {
-											case dba::tickSym::Nil =>
-												ResolvedEntity(dba, tickSym)
-										}
+	/**
+	*	instance of class akka.stream.scaladsl.Flow
+	*/
+	val transform1:Flow[RawEntity, ResolvedEntity, Unit] = Flow[RawEntity]
+		.map(c => c.record.split(" ").toList)
+		.map(fnx(_))
+			.collect {
+				case dba::tickSym::Nil =>
+					ResolvedEntity(dba, tickSym)
+			}
 
-	// val transform2 = Flow[RawEntity]
-	// 									.map(upCaseTickSym(_))
-
-	val persistResolvedEntities = Sink.foreach[ResolvedEntity] { entity =>
-		println(entity)
+	val persistResolvedEntities:Sink[ResolvedEntity, Future[Unit]] = {
+		Sink.foreach[ResolvedEntity] { entity =>
+			println(entity)
+		}
 	}
 
-	import FlowGraph.Implicits._
-
-	rawEntities
-		.via(transform1)
-		.runWith(persistResolvedEntities)
-		.andThen {
-			case _ =>
-				actorSystem.shutdown()
-				actorSystem.awaitTermination()
+	val persistTickSyms:Sink[ResolvedEntity, Future[Unit]] = {
+		Sink.foreach[ResolvedEntity] { entity =>
+			println(entity.tickSym)
 		}
+	}
+
+
+	val graph = FlowGraph.closed() { implicit builder: FlowGraph.Builder[Unit] =>
+		import FlowGraph.Implicits._
+		val bcast = builder.add(Broadcast[RawEntity](2))
+		rawEntities ~> bcast.in
+		bcast.out(0) ~> transform1 ~> persistResolvedEntities
+		bcast.out(1) ~> transform1 ~> persistTickSyms
+	}
+
+	/**
+	*	a RunnableGraph instance
+	*/
+	graph
+		.run()
+		.onComplete {
+			case Success(_) =>
+				actorSystem.shutdown()
+			case Failure(err) =>
+				println("failure: ${err.getMessage}")
+				actorSystem.shutdown()
+		}
+		// .onComplete(_ => actorSystem.shutdown())
+		// .onComplete {
+		// 	x match {
+		// 		case Success(v)		=> 		println("result!")
+		// 		case Failure(q)		=>		println("fail")
+		// 	}
+		// 	actorSystem.shutdown()
+		// }
+
+
+
+
+	// rawEntities
+	// 	.via(transform1)
+	// 	.runWith(persistResolvedEntities)
+	// 	.andThen {
+	// 		case _ =>
+	// 			actorSystem.shutdown()
+	// 			actorSystem.awaitTermination()
+	// 	}
 }
 
 
