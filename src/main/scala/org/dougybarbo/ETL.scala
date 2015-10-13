5683f54a2624079bff2e03feb5d55bc861a8884c
alexland <doug.ybarbo@gmail.com>
2015-10-13 13:25:12 -0700

    moved higher-order function predicates to ETL module

---
+++ src/main/scala/org/dougybarbo/ETL.scala
@@ -0,0 +1,138 @@
+
+package org.dougybarbo
+
+
+object ETL {
+
+	trait Record {
+		def c1: Int
+		def c2: Int
+	}
+
+	final case class RawDataLine(
+		c1:Int,
+		c2:Int,
+		c3:Int,
+		c4:Int
+	) extends Record
+
+	final case class RawDataLineSm(
+		idx: Int,
+		c1: Int
+	) extends Record
+
+	val fnx = () => for (i <- (1 to 4)) yield RND.nextInt(100)
+	val fnx0 = () => for (i <- (1 to 2)) yield RND.nextInt(100)
+
+	val fnx1 = (q:Vector[Int]) => q match {
+		case Vector(a, b, c, d) => RawDataLine(a, b, c, d)
+		case Vector(a, b) => RawDataLineSm(a, b)
+	}
+
+	val fnx2 = (q:Vector[Int]) => q match {
+		case Vector(a, b) => RawDataLineSm(a, b)
+	}
+
+	val rawDataIn: Source[RawDataLine, Unit] = Source(
+		(1 to 1000000)
+			.map(_ => fnx().toVector)
+			.map(c => fnx1(c))
+	)
+
+	val rawDataIn0: Source[RawDataLineSm, Unit] = Source(
+		(1 to 5)
+			.map(_ => fnx0().toVector)
+			.map(c => fnx2(c))
+	)
+
+	def twoX(r:Record):Record = {
+		r match {
+			case RawDataLineSm(a, b) => RawDataLineSm(a*2, b*2)
+			case RawDataLine(a, b, c, d) => RawDataLine(a*2, b*2, c*2, d*2)
+		}
+	}
+
+	def twoX(r:RawDataLineSm):RawDataLineSm = {
+		r match {
+			case RawDataLineSm(a, b) => RawDataLineSm(a*2, b*2)
+		}
+	}
+
+	def oneFifthX(r:RawDataLineSm):RawDataLineSm = {
+		r match {
+			case RawDataLineSm(a, b) => RawDataLineSm(a/5, b/5)
+		}
+	}
+
+	def rowSum(r:RawDataLineSm):Int = {
+		r match {
+			case RawDataLineSm(a, b) => a + b
+		}
+	}
+
+	def rowSum1(r:RawDataLine):Int = {
+		r match {
+			case RawDataLine(a, b, c, d) => a + b + c + d
+		}
+	}
+
+	def rowMul(r:RawDataLineSm):Int = {
+		r match {
+			case RawDataLineSm(a, b) => a * b
+		}
+	}
+
+	def joinRows(u:RawDataLineSm, v:RawDataLineSm) = {
+		val v1 = Vector(u.c1, u.c2, v.c1, v.c2)
+		v1 match {
+			case Vector(a, b, c, d) => RawDataLine(a, b, c, d)
+		}
+	}
+
+	// commify printed integer values
+	val formatter = java.text.NumberFormat.getIntegerInstance
+	val fmt = (v:Int) => formatter.format(v)
+
+	implicit val actorSystem = ActorSystem("entity-resolver")
+	import actorSystem.dispatcher
+	implicit val flowMaterializer = ActorMaterializer()
+
+	/**
+	*	'count': a reusable Flow that transforms each item in the stream into a
+	*	a '1'
+	*	'counter', the Sink, can fold over it to get a count of items in the stream
+	*/
+	val count: Flow[RawDataLine, Int, Unit] = Flow[RawDataLine].map(_ => 1)
+	val counter: Sink[ Int, Future[Int] ] = Sink.fold[Int, Int](0)(_ + _)
+
+	val toVec = (q:RawDataLine) => q match {
+		case RawDataLine(a, b, c, d) => Vector(a, b, c, d)
+	}
+
+	/**
+	*	transformation functions passed to higher-order
+	*	functions in the graph
+	*	variables named t followed by integer are anonymous
+	*	functions that transform individual data lines
+	*/
+	val t1 = (v:Vector[Int]) => v.map(_ / 2)
+	val t2 = (v:Vector[Int]) => v.filter(_ <= 25)
+
+	val lineSum1: Flow[RawDataLine, Int, Unit] = Flow[RawDataLine]
+		.map(c => toVec(c).sum)
+
+	val lineSum2: Flow[Vector[Int], Int, Unit] = Flow[Vector[Int]]
+			.map(c => c.sum)
+
+	val lineDiv: Flow[RawDataLine, Vector[Int], Unit] = Flow[RawDataLine]
+		.map(c => toVec(c))
+		.map(t1(_))
+
+	val lineFilter1: Flow[Vector[Int], Vector[Int], Unit] = Flow[Vector[Int]]
+		.map(t2(_))
+
+	val streamSum: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
+
+	val res = (g:RunnableGraph[Future[Int]]) => g.run()
+
+}
--- src/main/scala/org/dougybarbo/StreamGraphs.scala
+++ src/main/scala/org/dougybarbo/StreamGraphs.scala
@@ -33,118 +33,12 @@ import akka.util.ByteString
 import org.json4s.JsonAST.JString
 import org.json4s.jackson.JsonMethods._
 
+import org.dougybarbo.ETL._
+
 
 object Main {
 
 	def main(args: Array[String]): Unit = {
-
-		trait Record {
-			def c1: Int
-			def c2: Int
-		}
-
-		final case class RawDataLine(
-			idx:Int,
-			c1:Int,
-			c2:Int,
-			c3:Int
-		) extends Record
-
-		final case class RawDataLineSm(
-			idx: Int,
-			c1: Int
-		) extends Record
-
-		val fnx = () => for (i <- (1 to 4)) yield RND.nextInt(100)
-		val fnx0 = () => for (i <- (1 to 2)) yield RND.nextInt(100)
-
-		val fnx1 = (q:Vector[Int]) => q match {
-			case Vector(a, b, c, d) => RawDataLine(a, b, c, d)
-			case Vector(a, b) => RawDataLineSm(a, b)
-		}
-
-		val fnx2 = (q:Vector[Int]) => q match {
-			case Vector(a, b) => RawDataLineSm(a, b)
-		}
-
-		val rawDataIn: Source[RawDataLine, Unit] = Source(
-			(1 to 1000000)
-				.map(_ => fnx().toVector)
-				.map(c => fnx1(c))
-		)
-
-		val rawDataIn0: Source[RawDataLineSm, Unit] = Source(
-			(1 to 5)
-				.map(_ => fnx0().toVector)
-				.map(c => fnx2(c))
-		)
-
-		def twoX(r:Record):Record = {
-			r match {
-				case RawDataLineSm(a, b) => RawDataLineSm(a*2, b*2)
-				case RawDataLine(a, b, c, d) => RawDataLine(a*2, b*2, c*2, d*2)
-			}
-		}
-
-		def twoX(r:RawDataLineSm):RawDataLineSm = {
-			r match {
-				case RawDataLineSm(a, b) => RawDataLineSm(a*2, b*2)
-			}
-		}
-
-		def rowSum(r:RawDataLineSm):Int = {
-			r match {
-				case RawDataLineSm(a, b) => a + b
-			}
-		}
-
-
-		// commify printed integer values
-		val formatter = java.text.NumberFormat.getIntegerInstance
-		val fmt = (v:Int) => formatter.format(v)
-
-		implicit val actorSystem = ActorSystem("entity-resolver")
-		import actorSystem.dispatcher
-		implicit val flowMaterializer = ActorMaterializer()
-
-		/**
-		*	'count': a reusable Flow that transforms each item in the stream into a
-		*	a '1'
-		*	'counter', the Sink, can fold over it to get a count of items in the stream
-		*/
-		val count: Flow[RawDataLine, Int, Unit] = Flow[RawDataLine].map(_ => 1)
-		val counter: Sink[ Int, Future[Int] ] = Sink.fold[Int, Int](0)(_ + _)
-
-		val toVec = (q:RawDataLine) => q match {
-			case RawDataLine(a, b, c, d) => Vector(a, b, c, d)
-		}
-
-		/**
-		*	transformation functions passed to higher-order
-		*	functions in the graph
-		*	variables named t followed by integer are anonymous
-		*	functions that transform individual data lines
-		*/
-		val t1 = (v:Vector[Int]) => v.map(_ / 2)
-		val t2 = (v:Vector[Int]) => v.filter(_ <= 25)
-
-		val lineSum1: Flow[RawDataLine, Int, Unit] = Flow[RawDataLine]
-			.map(c => toVec(c).sum)
-
-		val lineSum2: Flow[Vector[Int], Int, Unit] = Flow[Vector[Int]]
-   			.map(c => c.sum)
-
-		val lineDiv: Flow[RawDataLine, Vector[Int], Unit] = Flow[RawDataLine]
-			.map(c => toVec(c))
-			.map(t1(_))
-
-		val lineFilter1: Flow[Vector[Int], Vector[Int], Unit] = Flow[Vector[Int]]
-			.map(t2(_))
-
-		val streamSum: Sink[Int, Future[Int]] = Sink.fold[Int, Int](0)(_ + _)
-
-		val res = (g:RunnableGraph[Future[Int]]) => g.run()
-
 		/**
 		*	4 RunnableGraph instances, all are
 		*	immutable, thread-safe, and freely shareable;
@@ -227,6 +121,58 @@ object Main {
 		}
 
 		g6.run()
+
+		val g7 = FlowGraph.closed() { implicit builder: FlowGraph.Builder[Unit] =>
+			import FlowGraph.Implicits._
+			val bcast = builder.add(Broadcast[RawDataLineSm](2))
+			// val merge = builder.add(Merge[Int](2))
+			val c0:Flow[RawDataLineSm, RawDataLineSm, Unit] = {
+				Flow[RawDataLineSm].map(twoX(_))
+			}
+			val c1:Flow[RawDataLineSm,Int,Unit] = Flow[RawDataLineSm]
+				.map(rowSum(_))
+			val c2:Flow[RawDataLineSm,Int,Unit] = Flow[RawDataLineSm]
+				.map(rowMul(_))
+			val dataOut1:Sink[Any,Future[Unit]] = Sink.foreach(println(_))
+			val dataOut2:Sink[Any,Future[Unit]] = Sink.foreach(println(_))
+
+			rawDataIn0 ~> c0 ~> bcast ~> c1 ~> dataOut1
+													bcast ~> c2 ~> dataOut2
+		}
+
+		g7.run()
+
+		// val g8 = FlowGraph.closed() { implicit builder: FlowGraph.Builder[Unit] =>
+		// 	import FlowGraph.Implicits._
+		// 	val bcast = builder.add(Broadcast[RawDataLineSm](2))
+		// 	val merge = builder.add(Zip[RawDataLineSm,RawDataLineSm])
+		//
+		// 	val c0:Flow[RawDataLineSm, RawDataLineSm, Unit] = {
+		// 		Flow[RawDataLineSm].map(twoX(_))
+		// 	}
+		// 	val c1:Flow[RawDataLineSm,RawDataLineSm,Unit] = Flow[RawDataLineSm]
+		// 		.map(oneFifthX(_))
+		// 	val c2:Flow[RawDataLineSm,RawDataLineSm,Unit] = Flow[RawDataLineSm]
+		// 		.map(twoX(_))
+		// 	// c3 concatanates 2 small records into a single big record
+		// 	val c3:Flow[RawDataLineSm,RawDataLine,Unit] = Flow[RawDataLineSm]
+		// 		.map(f1(_))
+		// 	// c4 row-wise sum of a big record
+		// 	val c4:Flow[RawDataLine,Int,Unit] = Flow[RawDataLine]
+		// 		.map(rowSum1(_))
+		// 	val dataOut:Sink[Any,Future[Unit]] = Sink.foreach(println(_))
+		// 	rawDataIn0 ~> c0 ~> bcast ~> c1 ~> merge ~> c3 ~> c4 ~> dataOut
+		// 											bcast ~> c2 ~> merge
+		// }
+		//
+		// g8.run()
+
+
+
+
+
+
+
 
 
 	}
